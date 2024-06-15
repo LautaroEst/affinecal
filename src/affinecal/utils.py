59224ae1ad099402cdd467fbb214bc1945b8a05d
LautaroEst <lautaro.est@gmail.com>
2024-06-15 19:51:35 -0300

    min_cal

--- src/affinecal/utils.py
+++ src/affinecal/utils.py
@@ -4,6 +4,7 @@ from .trainer import Trainer
 from .losses import init_loss
 import torch
 from sklearn.model_selection import StratifiedKFold, StratifiedGroupKFold, GroupKFold, KFold
+import numpy as np
 
 def calibrate_xval(logits, targets, seed=0, condition_ids=None, stratified=True, nfolds=5, **kwargs):
     logitscal = torch.zeros(logits.size())
@@ -20,8 +21,8 @@ def calibrate_xval(logits, targets, seed=0, condition_ids=None, stratified=True,
         else:
             skf = KFold(n_splits=nfolds, shuffle=True, random_state=seed)
 
-    alpha = kwargs.get("alpha", "scalar")
-    beta = kwargs.get("beta", True)
+    alpha = kwargs.pop("alpha", "scalar")
+    beta = kwargs.pop("beta", True)
     trainer = Trainer(**kwargs)
     for trni, tsti in skf.split(logits, targets, condition_ids):
         model = AffineCalibrator(num_classes=logits.shape[1], alpha=alpha, beta=beta)
@@ -31,16 +32,40 @@ def calibrate_xval(logits, targets, seed=0, condition_ids=None, stratified=True,
 
     return logitscal
 
+def train_on_test(logits, targets, **kwargs):
+    alpha = kwargs.pop("alpha", "scalar")
+    beta = kwargs.pop("beta", True)
+    trainer = Trainer(**kwargs)
+    model = AffineCalibrator(num_classes=logits.shape[1], alpha=alpha, beta=beta)
+    trainer.fit(model, logits, targets)
+    with torch.no_grad():
+        logitscal = torch.log_softmax(model(logits), dim=1)
+    if np.isnan(logitscal).any():
+        print(logits, targets)
+        print(model.alpha, model.beta)
+    return logitscal
+
+
 def cal_loss(logits, targets, relative=True, **kwargs):
     loss_name = kwargs.pop("loss", "cross_entropy")
     kwargs["loss"] = loss_name
-    logitscal = calibrate_xval(logits, targets, **kwargs)
+    # logitscal = calibrate_xval(logits, targets, **kwargs)
+    logitscal = train_on_test(logits, targets, **kwargs)
     loss_fn = init_loss(loss_name)
     start_loss = loss_fn(logits, targets)
     end_loss = loss_fn(logitscal, targets)
     if relative:
-        return (end_loss - start_loss) / start_loss
-    return end_loss - start_loss
+        return (start_loss - end_loss) / start_loss
+    return start_loss - end_loss
+
+def min_cal(logits, targets, **kwargs):
+    loss_name = kwargs.pop("loss", "cross_entropy")
+    kwargs["loss"] = loss_name
+    logitscal = calibrate_xval(logits, targets, **kwargs)
+    loss_fn = init_loss(loss_name)
+    end_loss = loss_fn(logitscal, targets)
+    return end_loss
+
 
     
 
